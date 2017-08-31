a7dcf2f550023d3d50e99e50e130abdd772fd4ac
dboazalead <dboudeau@azalead.com>
2017-08-31 22:29:57 +0200

    - Adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='speculator',
+      version='1.0',
+      description='Python Distribution',
+      author='dboudz',
+      url='https://github.com/dboudz/speculator',
+      packages=[],
+     )
